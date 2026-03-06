f316f723ee7b36c5a477cc5fdb63579f53fa8a98
iamati231 <sezginatakanisik@gmail.com>
2026-03-06 21:39:13 +0100

    Add Info.plist and fix Package.swift for proper app bundle

--- Package.swift
+++ Package.swift
@@ -1,19 +1,14 @@
-// swift-tools-version: 5.9
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "FitnessApp2",
     platforms: [.iOS(.v17)],
-    products: [
-        .library(name: "FitnessApp2", targets: ["FitnessApp2"])
-    ],
     targets: [
-        .target(
+        .executableTarget(
             name: "FitnessApp2",
             path: "Sources/FitnessApp2",
-            swiftSettings: [
-                .unsafeFlags(["-swift-version", "5"])
-            ]
+            resources: [.process("Resources")]
         )
     ]
 )
